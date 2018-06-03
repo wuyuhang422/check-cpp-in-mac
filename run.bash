10b01f1d9083f8117cb5050b548bb45ddfa62df4
RainAir <wuyuhang422@gmail.com>
2018-06-03 20:26:56 +0800

    init
    init

---
+++ run.bash
@@ -0,0 +1,13 @@
+g++ baoli.cpp -o baoli
+g++ compare.cpp -o compare
+g++ data.cpp -o data
+g++ std.cpp -o std
+# clear
+./compare
+rm -f baoli
+rm -f data
+rm -f std
+rm -f compare
+rm -f test.in
+rm -f test1.out
+rm -f test2.out
