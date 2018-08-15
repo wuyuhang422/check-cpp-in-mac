28d12abfe3cedb86e36e7ff39c98da9971582fa4
wuyuhang422 <wuyuhang422@gmail.com>
2018-08-15 11:00:11 +0800

    update

--- run.bash
+++ run.bash
@@ -1,7 +1,11 @@
-g++ baoli.cpp -o baoli
-g++ compare.cpp -o compare
-g++ data.cpp -o data
-g++ std.cpp -o std
+# g++ baoli.cpp -o baoli
+g++ baoli.cpp -o baoli -O2 -lm -DONLINE_JUDGE
+# g++ compare.cpp -o compare
+g++ compare.cpp -o compare -O2 -lm -DONLINE_JUDGE
+# g++ data.cpp -o data
+g++ data.cpp -o data -O2 -lm -DONLINE_JUDGE
+# g++ std.cpp -o std
+g++ std.cpp -o std -O2 -lm -DONLINE_JUDGE
 # clear
 ./compare
 rm -f baoli
